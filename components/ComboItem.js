bedda283a35f86f57c38d3c7f71332a00eb6c0d4
Roshan Thapa Magar <roshan@octspace.com>
2026-02-13 14:33:56 +0545

    hight

--- components/ComboItem.js
+++ components/ComboItem.js
@@ -6,7 +6,7 @@ export default function ComboItem({ item, className }) {
       key={item.id}
       className={`flex-shrink-0  border-2 border-gray-300 rounded-lg transition-all duration-300 ease-in-out hover:border-green-500 hover:shadow-lg cursor-pointer ${className}`}
     >
-      <div className="relative w-full rounded-t-md h-48 sm:h-56 md:h-60 lg:h-48 overflow-hidden">
+      <div className="relative w-full rounded-t-md h-32 md:h-48 overflow-hidden">
         <Image
           src={item.image}
           alt={item.title}
